d57217c236b12cff602f2cb14afc60262b6fba89
Akshay Basutkar <akshaybasutkar321@gmail.com>
2026-02-07 23:40:24 +0530

    Removed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
+const DATABASE_URL =
+  "postgresql://postgres:Bukar@localhost:5432/proctorStudent";
 
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: DATABASE_URL,
   },
 });
